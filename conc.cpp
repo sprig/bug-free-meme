86d6176527d8af79d756331cd8729ace1c1bd086
Konstantin Kliakhandler <kosta@slumpy.org>
2017-08-11 02:04:15 -0400

    Added license clauses.

--- conc.cpp
+++ conc.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2017 Konstantin Kliakhandler
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include"concordance.hpp"
 #include<iostream>
 #include<fstream>
@@ -5,35 +25,62 @@
 #include<string>
 
 int main(int argc, char** argv) {
-  kk::concordance::Concordance c;
-  std::istream* in;
+    std::istream* in;
+    kk::concordance::Concordance c;
 
-  switch (argc) {
-    // Read from standard input
-  case 1: {
-    // Don't skip reading whitespace
-    in = &std::cin;
-    break;
-  }
-    //Read from the given file
-  case 2: {
-    std::string filename = argv[1];
-    in = new std::ifstream(filename, std::ios_base::binary);
-    break;
-  }
-  default : {
-    std::cerr << "Usage: conc [filename]" << std::endl;
-    return 1;
-  }
-  }
+    switch (argc) {
+    case 1: {
+        // Read from standard input
 
-  *in >> std::noskipws;
-  for(std::istream_iterator<char> cit(*in), end = std::istream_iterator<char>(); cit != end; ++cit) {
-    c.next_byte(*cit);
-  }
+        in = &std::cin;
+        break;
+    }
+    case 2: {
+        // Read from the given file or sort by frequency
 
-  for (auto pair : c.counts()) {
-    std::cout << pair.first << " " << pair.second << std::endl;
-  }
-  return 0;
+        std::string arg = argv[1];
+        if (arg == std::string("-F")) {
+            // Read from input and sort by frequency
+            c.sort_by_frequency(true);
+            in = &std::cin;
+        } else {
+            // Read from file and sort by word
+            in = new std::ifstream(arg, std::ios_base::binary);
+        }
+        break;
+    }
+    case 3: {
+        // Read from the given file and sort by frequency
+
+        std::string arg = argv[1];
+        if (arg == std::string("-F")) {
+            c.sort_by_frequency(true);
+            std::string filename = argv[2];
+            in = new std::ifstream(filename, std::ios_base::binary);
+            break;
+        }
+    }
+    default : {
+        // Display help/usage
+        std::cerr << "Usage: conc [-F] [filename]" << std::endl;
+        return 1;
+    }
+    }
+
+    *in >> std::noskipws; // Don't skip reading whitespace
+    for(std::istream_iterator<char> cit(*in), end; cit != end; ++cit) {
+        c.next_byte(*cit);
+    }
+
+    if (!c.complete_codepoint_p()) {
+        std::cerr << "Incomplete UTF-8 codepoint at the end of output!";
+        return 1;
+    }
+
+    // Output the sorted values
+    for(auto pair : c) {
+        std::cout << pair.first << " " << pair.second << std::endl;
+    }
+
+    return 0;
 }
